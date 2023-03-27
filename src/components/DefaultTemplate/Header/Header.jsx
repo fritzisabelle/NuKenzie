4ed32e387c539aba02da3229a26b58ac42d51670
Isabelle <isabelle.bernardes@hotmail.com>
2023-03-27 11:26:01 -0300

    fix: img root

--- src/components/DefaultTemplate/Header/Header.jsx
+++ src/components/DefaultTemplate/Header/Header.jsx
@@ -1,16 +1,11 @@
 import styles from "./styles.module.css";
-import "../../../assets/Nu-Kenzie.svg";
+import logoNukenzie from "../../../assets/Nu-Kenzie.svg";
 
 export const Header = () => {
   return (
     <header>
       <div className={styles.headerContainer}>
-        <img
-          src="../../../assets/Nu-Kenzie.svg"
-          // src="../src/assets/Nu-Kenzie.svg"
-          alt="logo nukenzie"
-          className={styles.logo}
-        />
+        <img src={logoNukenzie} alt="logo nukenzie" className={styles.logo} />
       </div>
     </header>
   );
